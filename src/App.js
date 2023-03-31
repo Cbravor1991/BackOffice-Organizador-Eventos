a7cd5fcb7be6570a7b22d77d8d3759b86b9a677c
Cbravor1991 <cbravor@fi.uba.ar>
2023-03-31 18:12:48 -0300

    probando configuraciones

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import LoginPage from './routes/LoginPage';
 import HomePage from './routes/HomePage';
 import DashBoardPage from './routes/DashboardPage';
+import { Text, Image, View, Page, Document, PDFDownloadLink } from '@react-pdf/renderer';
 
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar';
