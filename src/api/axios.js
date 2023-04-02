922746bf1caaffd2787546cbe988986c628ff071
JulioCastillo1 <54676173+JulioCastillo1@users.noreply.github.com>
2023-04-02 08:58:15 -0300

    Actualizo base url del web host

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://web-admin.onrender.com/'
+    baseURL: 'https://web-organization-ticketappfiuba.vercel.app/home/'
     //baseURL: 'http://localhost:3000/'
 });
