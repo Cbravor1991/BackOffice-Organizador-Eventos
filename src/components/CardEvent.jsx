8d6799f384b95e328eea625ffb705df57aa5f031
Tomas Arrachea <tomas.arrachea@zerf.tech>
2023-05-02 00:43:51 -0300

    agrego padding en vista y creacion

--- src/components/CardEvent.jsx
+++ src/components/CardEvent.jsx
@@ -1,21 +1,16 @@
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 export default function CardEvent() {
   return (
-    <Box sx={{ mb: 4 }}>
-      <Card sx={{ backgroundColor: 'white', color: '#fff' }}>
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '10%', py: 4 }}>
-          <Typography variant="h6" component="div" sx={{ color: 'black', fontSize: 16, fontWeight: 700, mb: 2 }}>
-            Bienvenido a tus eventos
-          </Typography>
-          <Button href={'/loadEvent'} sx={{ fontFamily: "'Circular Std', Arial, sans-serif", fontSize: 14, fontWeight: 700, color: '#fff', backgroundColor: '#1286f7', borderRadius: 2, px: 4, py: 1, '&:hover': { backgroundColor: '#1286f7' } }}>
-            Crear evento
-          </Button>
-        </Box>
-      </Card>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '10%', py: 4 }}>
+      <Typography variant="h6" component="div" sx={{ color: 'black', fontSize: 16, fontWeight: 700, mb: 2 }}>
+        Bienvenido a tus eventos
+      </Typography>
+      <Button href={'/loadEvent'} sx={{ fontFamily: "'Circular Std', Arial, sans-serif", fontSize: 14, fontWeight: 700, color: '#fff', backgroundColor: '#1286f7', borderRadius: 2, px: 4, py: 1, '&:hover': { backgroundColor: '#1286f7' } }}>
+        Crear evento
+      </Button>
     </Box>
   );
 }
