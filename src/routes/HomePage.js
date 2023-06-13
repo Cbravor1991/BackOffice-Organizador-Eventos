f7ca8aafd711900878d52592809bfc75f2026dab
Cbravor1991 <cbravor@fi.uba.ar>
2023-06-13 04:29:54 -0300

    config deploy

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/HomePage.scss';
+import '../styles/HomePage.css';
 import { Link } from 'react-router-dom';
 import { useSpring, animated } from 'react-spring';
 import LoginButton from '../components/LoginButton'
