4336e4ad2d19d2148f72f539c747bb4c1bf3af22
JulioCastillo1 <jecastillo@fi.uba.ar>
2023-06-14 01:58:52 -0300

    Actualizo dependencia

--- src/routes/ShowsEvents.js
+++ src/routes/ShowsEvents.js
@@ -21,7 +21,7 @@ import { api } from '../api/axios';
 import Navbar from '../components/NavBar';
 import swal from 'sweetalert2';
 import { Button, Grid } from '@mui/material';
-import { Height } from '@material-ui/icons';
+import { Height } from '@mui/icons-material'';
 import Tooltip from '@mui/material/Tooltip';
 
 
